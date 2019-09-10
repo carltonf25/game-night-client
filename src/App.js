86f31ff44f6168e3a1b0f2552070776754391171
Carlton Freeman <carltonfreeman@carltons-mbp.lan>
2019-09-09 23:44:09 -0400

    fix filepath issues

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
-import Header from "/components/Header";
-import Footer from "/components/Footer";
-import BurgerNav from "/components/BurgerNav";
-import { AppContext } from "/AppContext";
-import routes from "/router";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import BurgerNav from "./components/BurgerNav";
+import { AppContext } from "./AppContext";
+import routes from "./router";
 import { useRoutes, useRedirect } from "hookrouter";
 import { useSpring } from "react-spring";
-import "/App.css";
+import "./App.css";
 
 const App = () => {
   const routeResult = useRoutes(routes);
